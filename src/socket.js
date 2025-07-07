00b12f2755dfdb38d7d125c940ee53525446a358
Itai Segal <segai@post.bgu.ac.il>
2025-07-07 07:35:28 +0300

    Validate REACT_APP_BACKEND_URL and export socket instance in socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,14 +1,18 @@
 import io from 'socket.io-client';
 
-const SOCKET_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001';
+if (!process.env.REACT_APP_BACKEND_URL) {
+  throw new Error('REACT_APP_BACKEND_URL is not defined');
+}
 
-export const socket = io(SOCKET_URL, {
+const socket = io(process.env.REACT_APP_BACKEND_URL, {
   transports: ['websocket'],
   reconnection: true,
   reconnectionDelay: 1000,
   reconnectionDelayMax: 5000,
   reconnectionAttempts: 5
 });
+
+export default socket;
 
 // Debug logging in development
 if (process.env.NODE_ENV === 'development') {
